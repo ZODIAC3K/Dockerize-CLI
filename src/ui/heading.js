a3c60654b609a2d98767a7ee2b545e4d98670f64
zodiac3k <harshdeepanshustrix@gmail.com>
2024-10-07 13:22:23 +0530

    updated .gitignore, package.json, added src/fonts/Standard.flf for figlet.

--- src/ui/heading.js
+++ src/ui/heading.js
@@ -1,4 +1,7 @@
 import figlet from "figlet";
+import { readFileSync } from "fs";
+
+figlet.parseFont("standard", readFileSync("./src/fonts/standard.flf", "utf8"));
 
 let heading = figlet.textSync("Dockerize-CLI", {
 	font: "standard",
